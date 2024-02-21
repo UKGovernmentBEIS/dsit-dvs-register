f2a58921fa1df324074b0fa0fe1c156a41d587b6
Aiswarya Rajendran <Aiswarya.Rajendran@dsit.gov.uk>
2024-02-21 17:28:21 +0000

    feat(dbhealthcheck): db connection success message

--- DVSRegister.CommonUtility/Constants.cs
+++ DVSRegister.CommonUtility/Constants.cs
@@ -5,6 +5,6 @@
         public const string ConnectionStringBuiler = "Host={0};Username={1};Password={2};Database={3}";
         public const string DbContextNull = "Db context is null";
         public const string DbConnectionFailed = "DB connection failed:";
-        public const string DbConnectionSuccess = "DB connection failed:";
+        public const string DbConnectionSuccess = "DB connection success:";
     }
 }
