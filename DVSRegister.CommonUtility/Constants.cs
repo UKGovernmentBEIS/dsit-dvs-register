d5826005ab0bb27b60737411199986edaa5ad448
Aiswarya Rajendran <Aiswarya.Rajendran@dsit.gov.uk>
2024-02-21 17:24:40 +0000

    feat(dbhealthcheck):verify connection to aws postgresql

--- DVSRegister.CommonUtility/Constants.cs
+++ DVSRegister.CommonUtility/Constants.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DVSRegister.CommonUtility
+﻿namespace DVSRegister.CommonUtility
 {
     public static class Constants
     {
+        public const string ConnectionStringBuiler = "Host={0};Username={1};Password={2};Database={3}";
+        public const string DbContextNull = "Db context is null";
+        public const string DbConnectionFailed = "DB connection failed:";
+        public const string DbConnectionSuccess = "DB connection failed:";
     }
 }
