25dcd549784415b1be9032a1f96163688cb18c75
Aiswarya Rajendran <Aiswarya.Rajendran@dsit.gov.uk>
2024-03-06 10:37:01 +0000

    feat(notify): utility method to integrate with govuk.notify

---
+++ DVSRegister.CommonUtility/Email/IEmailSender.cs
@@ -0,0 +1,8 @@
+﻿namespace DVSRegister.CommonUtility.Email
+{
+    public interface IEmailSender
+    {
+        //Add method definition for sending customized emails and implement in GovUkNotifyApi
+
+    }
+}
