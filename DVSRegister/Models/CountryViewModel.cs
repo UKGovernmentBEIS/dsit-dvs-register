7492abc27654a8f4a5bbd8e18b813c0c6a48d962
Aiswarya Rajendran <Aiswarya.Rajendran@dsit.gov.uk>
2024-04-05 15:39:09 +0100

    feat(pre-registration): validation for company and country pages

--- DVSRegister/Models/CountryViewModel.cs
+++ DVSRegister/Models/CountryViewModel.cs
@@ -1,13 +1,16 @@
 ﻿using DVSRegister.BusinessLogic.Models.PreRegistration;
-
+using DVSRegister.Validations;
+using System.ComponentModel.DataAnnotations;
 namespace DVSRegister.Models
 {
     public class CountryViewModel
     {
         public bool? IsApplicationSponsor { get; set; }
-        public List<CountryDto> AvailableCountries { get; set; }       
-        public List<int> SelectedCountryIds { get; set; }
-        public List<CountryDto> SelectedCountries { get; set; }
+        public List<CountryDto>? AvailableCountries { get; set; }
+
+        [EnsureMinimumCount (ErrorMessage = "Select atleast once country your company trades in.")]
+        public List<int>? SelectedCountryIds { get; set; }
+        public List<CountryDto>? SelectedCountries { get; set; }
         public bool FromSummaryPage { get; set; }
     }
 }
---
+++ DVSRegister/Validations/RequiredIfAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DVSRegister.Validations
+{
+    public class RequiredIfAttribute : ValidationAttribute
+    {
+        private readonly string propertyName;
+        private readonly object desiredValue;
+
+        public RequiredIfAttribute(string propertyName, object desiredValue)
+        {
+            this.propertyName = propertyName;
+            this.desiredValue = desiredValue;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var propertyInfo = validationContext.ObjectType.GetProperty(propertyName);
+            if (propertyInfo == null)
+                return new ValidationResult($"Unknown property: {propertyName}");
+
+            var propertyValue = propertyInfo.GetValue(validationContext.ObjectInstance, null);
+
+            if (propertyValue?.ToString() == desiredValue.ToString() && string.IsNullOrEmpty(value?.ToString()))
+                return new ValidationResult(ErrorMessage);
+
+            return ValidationResult.Success;
+        }
+    }
+}
