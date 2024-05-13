2d73d10d666b8213f7cfc5d8b8f0615fca50d8a7
qumberhussain <qumber.hussain@gmail.com>
2024-05-13 14:10:29 +0100

    feat(cab-portal): add onboarding pages

--- DVSRegister/Controllers/LoginController.cs
+++ DVSRegister/Controllers/LoginController.cs
@@ -1,14 +1,52 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DVSRegister.Models.CAB;
+using DVSRegister.Extensions;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DVSRegister.Controllers
 {
-    [Route("cab-registration")]
+    [Route("cab-registration-login")]
     public class LoginController : Controller
     {
+        [HttpGet("")]
+        public IActionResult StartPage()
+        {
+            return View("StartPage");
+        }
         [HttpGet("sign-up")]
         public IActionResult SignUp()
         {
-            return View();
+            return View("SignUp");
+        }
+        [HttpGet("enter-code")]
+        public IActionResult EnterCode()
+        {
+            return View("EnterCode");
+        }
+        [HttpGet("mfa-registration")]
+        public IActionResult MFARegistration()
+        {
+            MFARegistrationViewModel MFARegistrationViewModel = new MFARegistrationViewModel();
+            return View("MFARegistration", MFARegistrationViewModel);
+        }
+        [HttpGet("create-password")]
+        public IActionResult CreatePassword()
+        {
+            return View("CreatePassword");
+        }
+        [HttpGet("sign-up-complete")]
+        public IActionResult SignUpComplete()
+        {
+            return View("SignUpComplete");
+        }
+        [HttpGet("login")]
+        public IActionResult LoginPage()
+        {
+            return View("LoginPage");
+        }
+        [HttpGet("mfa-confirmation")]
+        public IActionResult MFAConfirmation()
+        {
+            return View("MFAConfirmation");
         }
     }
 }
