97b6e10ff00f798c45a818fd71a8aea65bf2514b
Aiswarya Rajendran <Aiswarya.Rajendran@dsit.gov.uk>
2024-05-13 11:52:00 +0100

    feat(emailnotification): email to dip on submission

--- DVSRegister.CommonUtility/Models/GovUkNotifyConfiguration.cs
+++ DVSRegister.CommonUtility/Models/GovUkNotifyConfiguration.cs
@@ -4,7 +4,10 @@
     {
         public const string ConfigSection = "GovUkNotify";
         public string ApiKey { get; set; }
+        public string OfDiaEmailId { get; set; }
+        public string LoginLink { get; set; }
 
         public EmailConfirmationTemplate EmailConfirmationTemplate { get; set; }
+        public ApplicationReceivedTemplate ApplicationReceivedTemplate { get; set; }
     }
 }
