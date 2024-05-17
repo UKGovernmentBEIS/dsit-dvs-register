eab384e6177e9a0abc6eebc998973ec4feb5b3b3
Aiswarya Rajendran <Aiswarya.Rajendran@dsit.gov.uk>
2024-05-17 12:11:17 +0100

    feat(emailnotification) : cab email notifications

---
+++ DVSRegister.CommonUtility/Models/CabFailedLoginAttemptTemplate.cs
@@ -0,0 +1,8 @@
+﻿namespace DVSRegister.CommonUtility.Models
+{
+    public class CabFailedLoginAttemptTemplate
+    {
+        public string Id { get; set; }
+        public string Timestamp { get; set; }      
+    }
+}
--- DVSRegister.CommonUtility/Models/GovUkNotifyConfiguration.cs
+++ DVSRegister.CommonUtility/Models/GovUkNotifyConfiguration.cs
@@ -6,8 +6,15 @@
         public string ApiKey { get; set; }
         public string OfDiaEmailId { get; set; }
         public string LoginLink { get; set; }
+        public string CabSignUpLink { get; set; }
+        public string CabLoginLink { get; set; }
 
         public EmailConfirmationTemplate EmailConfirmationTemplate { get; set; }
         public ApplicationReceivedTemplate ApplicationReceivedTemplate { get; set; }
+        public CabAccountCreatedTemplate CabAccountCreatedTemplate { get; set; }
+        public CabFailedLoginAttemptTemplate CabFailedLoginAttemptTemplate { get; set; }
+
+        public CabInformationSubmittedTemplate CabInformationSubmittedTemplate { get; set; }
+        public CabSignUpActivationTemplate CabSignUpActivationTemplate { get; set; }    
     }
 }
