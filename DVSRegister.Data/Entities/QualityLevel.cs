095c0c644f38fdc12bb14f64a99cfe4af30a7745
Aiswarya Rajendran <Aiswarya.Rajendran@dsit.gov.uk>
2024-08-08 22:16:54 +0100

    feat(dbchanges): new tables for cab service

---
+++ DVSRegister.Data/Entities/QualityLevel.cs
@@ -0,0 +1,17 @@
+﻿using DVSRegister.CommonUtility.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DVSRegister.Data.Entities
+{
+    public class QualityLevel
+    {
+        public QualityLevel() { }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Level { get; set; }
+        public QualityTypeEnum QualityType { get; set; }
+    }
+}
