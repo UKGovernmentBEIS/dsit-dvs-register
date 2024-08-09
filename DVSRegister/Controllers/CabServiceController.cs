9c9a3f3c0d835b28a2ab5bfca6517cd9b9a1ca41
Naghma Khan <naghma.khan@ie.ey.com>
2024-08-09 14:53:42 +0100

    feat(cab): add new cab service controllers and views

---
+++ DVSRegister/Controllers/CabServiceController.cs
@@ -0,0 +1,36 @@
+﻿using DVSRegister.BusinessLogic.Models.CAB;
+using DVSRegister.BusinessLogic.Models.PreRegistration;
+using DVSRegister.BusinessLogic.Services.CAB;
+using DVSRegister.CommonUtility;
+using DVSRegister.CommonUtility.Email;
+using DVSRegister.CommonUtility.Models;
+using DVSRegister.CommonUtility.Models.Enums;
+using DVSRegister.Extensions;
+using DVSRegister.Models.CAB;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Security.Claims;
+
+
+namespace DVSRegister.Controllers
+{
+    [Route("cab-service")]
+    //[ValidCognitoToken]//
+    public class CabServiceController : Controller
+    {
+
+        private readonly ICabService cabService;
+
+        public CabServiceController(ICabService cabService)
+        {
+            this.cabService = cabService;
+        }
+
+        [HttpGet("before-you-start")]
+        public IActionResult BeforeYouStart()
+        {
+
+            return View();
+        }
+    }
+}
