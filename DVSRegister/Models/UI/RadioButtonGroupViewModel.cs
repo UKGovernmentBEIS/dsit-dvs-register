7fa11582967b0983123df71a82596e600b058464
Aiswarya Rajendran <Aiswarya.Rajendran@dsit.gov.uk>
2024-08-27 11:48:45 +0100

    fix(radio-button): added field set legend in radio buttons

--- DVSRegister/Models/UI/RadioButtonGroupViewModel.cs
+++ DVSRegister/Models/UI/RadioButtonGroupViewModel.cs
@@ -1,9 +1,14 @@
-﻿namespace DVSRegister.Models
+﻿using DVSRegister.Models.UI.Enums;
+
+namespace DVSRegister.Models
 {
     public class TwoRadioButtonGroupViewModel
     {
         public string PropertyName { get; set; }
         public bool? Value { get; set; }
+        public string FieldSetLegend { get; set; }
+        public HeadingEnum Heading { get; set; }
+        public string HeadingClass { get; set; }
         public string ErrorMessage { get; set; }
         public bool HasError { get; set; }
         public string Label1 { get; set; }
