1291c8c576c8a79109132dc504c804bda11b57f8
Aiswarya Rajendran <Aiswarya.Rajendran@dsit.gov.uk>
2024-09-05 09:52:56 +0100

    feat(db-change): publicinteresrcheck table added

--- DVSRegister.CommonUtility/Models/Enums/ServiceStatusEnum.cs
+++ DVSRegister.CommonUtility/Models/Enums/ServiceStatusEnum.cs
@@ -6,7 +6,7 @@ namespace DVSRegister.CommonUtility.Models
     {
         //Donot change order of the enum as the ids are used to save in database
         //New entries should be added at the last
-        [Description("Submitted")]
+        [Description("Submitted")] //status when submitted by cab
         Submitted = 1,
         [Description("Received")] // Status when provider consents to proceed application, opening loop
         Received = 2,
