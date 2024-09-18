54467088be4c4c957df05a5c601d82ed496af22f
Aiswarya Rajendran <Aiswarya.Rajendran@dsit.gov.uk>
2024-09-18 12:24:49 +0100

    feat(db-change) : add rejectionreasons column

---
+++ DVSRegister.Data/Migrations/20240918112301_RejectionReasonsColumn.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DVSRegister.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RejectionReasonsColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RejectionReasons",
+                table: "PublicInterestCheck",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Cab",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedTime",
+                value: new DateTime(2024, 9, 18, 11, 23, 0, 918, DateTimeKind.Utc).AddTicks(6619));
+
+            migrationBuilder.UpdateData(
+                table: "Cab",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedTime",
+                value: new DateTime(2024, 9, 18, 11, 23, 0, 918, DateTimeKind.Utc).AddTicks(6622));
+
+            migrationBuilder.UpdateData(
+                table: "Cab",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedTime",
+                value: new DateTime(2024, 9, 18, 11, 23, 0, 918, DateTimeKind.Utc).AddTicks(6623));
+
+            migrationBuilder.UpdateData(
+                table: "Cab",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedTime",
+                value: new DateTime(2024, 9, 18, 11, 23, 0, 918, DateTimeKind.Utc).AddTicks(6624));
+
+            migrationBuilder.UpdateData(
+                table: "Cab",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "CreatedTime",
+                value: new DateTime(2024, 9, 18, 11, 23, 0, 918, DateTimeKind.Utc).AddTicks(6625));
+
+            migrationBuilder.UpdateData(
+                table: "Cab",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "CreatedTime",
+                value: new DateTime(2024, 9, 18, 11, 23, 0, 918, DateTimeKind.Utc).AddTicks(6626));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RejectionReasons",
+                table: "PublicInterestCheck");
+
+            migrationBuilder.UpdateData(
+                table: "Cab",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedTime",
+                value: new DateTime(2024, 9, 18, 11, 20, 45, 306, DateTimeKind.Utc).AddTicks(4761));
+
+            migrationBuilder.UpdateData(
+                table: "Cab",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedTime",
+                value: new DateTime(2024, 9, 18, 11, 20, 45, 306, DateTimeKind.Utc).AddTicks(4764));
+
+            migrationBuilder.UpdateData(
+                table: "Cab",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedTime",
+                value: new DateTime(2024, 9, 18, 11, 20, 45, 306, DateTimeKind.Utc).AddTicks(4765));
+
+            migrationBuilder.UpdateData(
+                table: "Cab",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedTime",
+                value: new DateTime(2024, 9, 18, 11, 20, 45, 306, DateTimeKind.Utc).AddTicks(4767));
+
+            migrationBuilder.UpdateData(
+                table: "Cab",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "CreatedTime",
+                value: new DateTime(2024, 9, 18, 11, 20, 45, 306, DateTimeKind.Utc).AddTicks(4768));
+
+            migrationBuilder.UpdateData(
+                table: "Cab",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "CreatedTime",
+                value: new DateTime(2024, 9, 18, 11, 20, 45, 306, DateTimeKind.Utc).AddTicks(4769));
+        }
+    }
+}
