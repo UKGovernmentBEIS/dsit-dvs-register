bb9fb32a23840fb0cc96e6da12683d6fe0457b49
Tim Saunders <42434851+timsaundersEY@users.noreply.github.com>
2024-10-16 13:35:28 +0100

    fix(filters): fix typo in FromDetailsPage filter

--- DVSRegister/Models/Register/Filters.cs
+++ DVSRegister/Models/Register/Filters.cs
@@ -4,7 +4,7 @@
     {
         public List<int> SelectedRoleIds { get; set; }
         public List<int> SelectedSupplementarySchemeIds { get; set; }
-        public bool FromDeatilsPage { get; set; }
+        public bool FromDetailsPage { get; set; }
         public int RemoveRole { get; set; }
         public int RemoveScheme { get; set; }
         public string SearchAction  { get; set; }
