135a35d86ad3e458937e55a7dd81d508da7cbec4
Naghma Khan <naghma.khan@ie.ey.com>
2024-10-18 09:20:57 +0100

    dvs-46:Clickjacking implementation

---
+++ DVSRegister/Middleware/SecurityHeadersMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http.Headers;
+using System.Security.Policy;
+using System.Text;
+using DVSRegister.CommonUtility;
+using Microsoft.Extensions.Options;
+
+namespace DVSRegister.Middleware
+{
+    public class SecurityHeadersMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public SecurityHeadersMiddleware(RequestDelegate next)
+        {
+            _next = next;//Storing the reference to next middleware in pipeline
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (!context.Response.HasStarted)
+            {
+                // Added security headers
+                context.Response.Headers["X-Frame-Options"] = "DENY";
+                context.Response.Headers["Content-Security-Policy"] = "frame-ancestors 'none'";
+                Console.WriteLine("Security headers added");
+            }
+
+            // Calling the next middleware in the pipeline
+            await _next(context);
+        }
+    }
+}
--- DVSRegister/Startup.cs
+++ DVSRegister/Startup.cs
@@ -29,6 +29,17 @@ namespace DVSRegister
             this.webHostEnvironment = webHostEnvironment;
         }
 
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseMiddleware<SecurityHeadersMiddleware>();
+            app.UseStaticFiles();
+            app.UseMvc();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
         public void ConfigureServices(IServiceCollection services)
         {
 
@@ -165,10 +176,10 @@ namespace DVSRegister
                 options.Cookie.Name = "Antiforgery";
                 options.Cookie.HttpOnly = true;
                 options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+                options.Cookie.SameSite = SameSiteMode.Strict;
             });
             services.AddSingleton<CookieService>();
+            services.AddMvc();
         }
-       
-
     }
 }
