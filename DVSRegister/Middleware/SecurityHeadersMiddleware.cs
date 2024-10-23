fff3c4816e5d94347283718c41658eb15ebf4d9e
Naghma Khan <naghma.khan@ie.ey.com>
2024-10-23 09:19:19 +0100

    feat(DVS-45):Implemented a strict header CSP policy

--- DVSRegister/Middleware/SecurityHeadersMiddleware.cs
+++ DVSRegister/Middleware/SecurityHeadersMiddleware.cs
@@ -2,6 +2,7 @@
 using System.Security.Policy;
 using System.Text;
 using DVSRegister.CommonUtility;
+using Microsoft.AspNetCore.Html;
 using Microsoft.Extensions.Options;
 
 namespace DVSRegister.Middleware
@@ -9,6 +10,7 @@ namespace DVSRegister.Middleware
     public class SecurityHeadersMiddleware
     {
         private readonly RequestDelegate _next;
+        public const string NonceContextKey = "CSP-Nonce";
         public SecurityHeadersMiddleware(RequestDelegate next)
         {
             _next = next;//Storing the reference to next middleware in pipeline
@@ -16,15 +18,34 @@ namespace DVSRegister.Middleware
 
         public async Task InvokeAsync(HttpContext context)
         {
+            // Generate a nonce for the current request
+            var nonce = Guid.NewGuid().ToString("N");
+            context.Items[NonceContextKey] = nonce;
+
             if (!context.Response.HasStarted)
             {
                 // Added security headers
                 context.Response.Headers["X-Frame-Options"] = "DENY";
-                context.Response.Headers["Content-Security-Policy"] = "frame-ancestors 'none'";
-            }
 
+                //CSP with nonce for inline scripts
+                context.Response.Headers["Content-Security-Policy"] =
+                $"script-src 'nonce-{nonce}' 'unsafe-inline' 'self' https:; " +
+                "connect-src 'self'; " +
+                "img-src 'self'; " +
+                "style-src 'self'; " +
+                "base-uri 'self'; " +
+                "font-src 'self'; " +
+                "form-action 'self';";
+            }
             // Calling the next middleware in the pipeline
             await _next(context);
+        }
+    }
+    public static class SecurityContextExtensions
+    {
+        public static HtmlString GetScriptNonce(this HttpContext context)
+        {
+            return new HtmlString((string)context.Items[SecurityHeadersMiddleware.NonceContextKey] ?? "");
         }
     }
 }
