235dc97d20eb1ea78abcca8fc99278bc2c81863a
Aiswarya Rajendran <Aiswarya.Rajendran@ie.ey.com>
2024-11-22 12:46:00 +0000

    feat(dbchanges) : make certificate review table columns nullable

--- DVSRegister.Data/Migrations/20241121162337_ModifyCertificateReviewTable.Designer.cs
+++ DVSRegister.Data/Migrations/20241121162337_ModifyCertificateReviewTable.Designer.cs
@@ -4,6 +4,7 @@ using System.Text.Json;
 using DVSRegister.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using NpgsqlTypes;
@@ -13,9 +14,11 @@ using NpgsqlTypes;
 namespace DVSRegister.Data.Migrations
 {
     [DbContext(typeof(DVSRegisterDbContext))]
-    partial class DVSRegisterDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241121162337_ModifyCertificateReviewTable")]
+    partial class ModifyCertificateReviewTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
