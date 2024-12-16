f4fb00b11db1a2992da1f7bab3abfba063c112da
Aiswarya Rajendran <Aiswarya.Rajendran@ie.ey.com>
2024-12-16 13:39:22 +0000

    fix(db-changes) : remove search vector column

--- DVSRegister.Data/Entities/Service.cs
+++ DVSRegister.Data/Entities/Service.cs
@@ -40,8 +40,7 @@ namespace DVSRegister.Data.Entities
         public CabUser CabUser { get; set; }
         public int ServiceNumber { get;set; }
       
-        public ServiceStatusEnum ServiceStatus { get; set; }
-        public NpgsqlTsVector SearchVector { get; set; }
+        public ServiceStatusEnum ServiceStatus { get; set; }      
         public DateTime? CreatedTime { get; set; }
         public DateTime? ModifiedTime { get; set; }
         public DateTime? PublishedTime { get; set; }
