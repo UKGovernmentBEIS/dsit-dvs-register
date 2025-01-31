f5a4b3d602aba38e83c28fd300a5e4782dfd5ff7
Aiswarya Rajendran <Aiswarya.Rajendran@ie.ey.com>
2025-01-31 11:11:30 +0000

    feat(db-changes):  nullable service tables columns

---
+++ DVSRegister.Data/Migrations/20250131110654_ServiceTableNullableColumns.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DVSRegister.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ServiceTableNullableColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "WebSiteAddress",
+                table: "Service",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ServiceName",
+                table: "Service",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "HasSupplementarySchemes",
+                table: "Service",
+                type: "boolean",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "boolean");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "HasGPG45",
+                table: "Service",
+                type: "boolean",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "boolean");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "HasGPG44",
+                table: "Service",
+                type: "boolean",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "boolean");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "FileSizeInKb",
+                table: "Service",
+                type: "numeric(10,1)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(10,1)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FileName",
+                table: "Service",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FileLink",
+                table: "Service",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ConformityIssueDate",
+                table: "Service",
+                type: "timestamp without time zone",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ConformityExpiryDate",
+                table: "Service",
+                type: "timestamp without time zone",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CompanyAddress",
+                table: "Service",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.UpdateData(
+                table: "Cab",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedTime",
+                value: new DateTime(2025, 1, 31, 11, 6, 53, 707, DateTimeKind.Utc).AddTicks(4959));
+
+            migrationBuilder.UpdateData(
+                table: "Cab",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedTime",
+                value: new DateTime(2025, 1, 31, 11, 6, 53, 707, DateTimeKind.Utc).AddTicks(4962));
+
+            migrationBuilder.UpdateData(
+                table: "Cab",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedTime",
+                value: new DateTime(2025, 1, 31, 11, 6, 53, 707, DateTimeKind.Utc).AddTicks(4963));
+
+            migrationBuilder.UpdateData(
+                table: "Cab",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedTime",
+                value: new DateTime(2025, 1, 31, 11, 6, 53, 707, DateTimeKind.Utc).AddTicks(4964));
+
+            migrationBuilder.UpdateData(
+                table: "Cab",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "CreatedTime",
+                value: new DateTime(2025, 1, 31, 11, 6, 53, 707, DateTimeKind.Utc).AddTicks(4965));
+
+            migrationBuilder.UpdateData(
+                table: "Cab",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "CreatedTime",
+                value: new DateTime(2025, 1, 31, 11, 6, 53, 707, DateTimeKind.Utc).AddTicks(4966));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "WebSiteAddress",
+                table: "Service",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ServiceName",
+                table: "Service",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "HasSupplementarySchemes",
+                table: "Service",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "HasGPG45",
+                table: "Service",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "HasGPG44",
+                table: "Service",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "FileSizeInKb",
+                table: "Service",
+                type: "numeric(10,1)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(10,1)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FileName",
+                table: "Service",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FileLink",
+                table: "Service",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ConformityIssueDate",
+                table: "Service",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ConformityExpiryDate",
+                table: "Service",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CompanyAddress",
+                table: "Service",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Cab",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedTime",
+                value: new DateTime(2025, 1, 21, 10, 41, 8, 288, DateTimeKind.Utc).AddTicks(8647));
+
+            migrationBuilder.UpdateData(
+                table: "Cab",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedTime",
+                value: new DateTime(2025, 1, 21, 10, 41, 8, 288, DateTimeKind.Utc).AddTicks(8650));
+
+            migrationBuilder.UpdateData(
+                table: "Cab",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedTime",
+                value: new DateTime(2025, 1, 21, 10, 41, 8, 288, DateTimeKind.Utc).AddTicks(8652));
+
+            migrationBuilder.UpdateData(
+                table: "Cab",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedTime",
+                value: new DateTime(2025, 1, 21, 10, 41, 8, 288, DateTimeKind.Utc).AddTicks(8654));
+
+            migrationBuilder.UpdateData(
+                table: "Cab",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "CreatedTime",
+                value: new DateTime(2025, 1, 21, 10, 41, 8, 288, DateTimeKind.Utc).AddTicks(8655));
+
+            migrationBuilder.UpdateData(
+                table: "Cab",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "CreatedTime",
+                value: new DateTime(2025, 1, 21, 10, 41, 8, 288, DateTimeKind.Utc).AddTicks(8657));
+        }
+    }
+}
