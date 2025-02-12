9f355ad85fb5c3c10a1d2902e5afe263021ebbd1
Aiswarya Rajendran <Aiswarya.Rajendran@dsit.gov.uk>
2025-02-12 16:19:31 +0000

    fix(db-changes) : fixed cab created time fix

---
+++ DVSRegister.Data/Migrations/20250212161411_FixCabCreatedTimeColumn.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DVSRegister.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixCabCreatedTimeColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Cab",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedTime",
+                value: new DateTime(2024, 9, 16, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "Cab",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedTime",
+                value: new DateTime(2024, 9, 16, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "Cab",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedTime",
+                value: new DateTime(2024, 9, 16, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "Cab",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedTime",
+                value: new DateTime(2024, 9, 16, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "Cab",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "CreatedTime",
+                value: new DateTime(2024, 9, 16, 0, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "Cab",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "CreatedTime",
+                value: new DateTime(2024, 9, 16, 0, 0, 0, 0, DateTimeKind.Utc));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Cab",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedTime",
+                value: new DateTime(2025, 1, 31, 11, 6, 53, 707, DateTimeKind.Utc).AddTicks(4959));
+
+            migrationBuilder.UpdateData(
+                table: "Cab",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedTime",
+                value: new DateTime(2025, 1, 31, 11, 6, 53, 707, DateTimeKind.Utc).AddTicks(4962));
+
+            migrationBuilder.UpdateData(
+                table: "Cab",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedTime",
+                value: new DateTime(2025, 1, 31, 11, 6, 53, 707, DateTimeKind.Utc).AddTicks(4963));
+
+            migrationBuilder.UpdateData(
+                table: "Cab",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedTime",
+                value: new DateTime(2025, 1, 31, 11, 6, 53, 707, DateTimeKind.Utc).AddTicks(4964));
+
+            migrationBuilder.UpdateData(
+                table: "Cab",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "CreatedTime",
+                value: new DateTime(2025, 1, 31, 11, 6, 53, 707, DateTimeKind.Utc).AddTicks(4965));
+
+            migrationBuilder.UpdateData(
+                table: "Cab",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "CreatedTime",
+                value: new DateTime(2025, 1, 31, 11, 6, 53, 707, DateTimeKind.Utc).AddTicks(4966));
+        }
+    }
+}
