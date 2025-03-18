42536a815b79285fb0d08659bcf1bd9b47275099
Kobithasan Vasantharajah <kobithasan.vasantharajah@uk.ey.com>
2025-03-18 09:43:05 +0000

    fix(controllers): updated error error logging

--- DVSRegister/Controllers/ErrorController.cs
+++ DVSRegister/Controllers/ErrorController.cs
@@ -1,4 +1,5 @@
-﻿using DVSRegister.Models.Error;
+﻿using DVSRegister.CommonUtility;
+using DVSRegister.Models.Error;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DVSRegister.Controllers
@@ -6,10 +7,18 @@ namespace DVSRegister.Controllers
     [Route("")]
     public class ErrorController : Controller
     {       
+        private readonly ILogger<ErrorController> _logger;
+        
+        public ErrorController(ILogger<ErrorController> logger)
+        {
+            _logger = logger;
+        }
 
         [HttpGet("cab-service/service-error")]
         public IActionResult CabHandleException()
         {
+            _logger.LogError("{Message}", Helper.LoggingHelper.FormatErrorMessage("Cab Service encountered an error."));
+
             ErrorViewModel errorViewModel = new ErrorViewModel { ApplicationType = "cab" };
             HttpContext?.Session.Clear();
             return View("ServiceIssue", errorViewModel);
@@ -18,6 +27,8 @@ namespace DVSRegister.Controllers
         [HttpGet("service-error")]
         public IActionResult ServiceError()
         {
+            _logger.LogError("{Message}", Helper.LoggingHelper.FormatErrorMessage("A general service error occurred."));
+
             ErrorViewModel errorViewModel = new ErrorViewModel { ApplicationType = string.Empty };
             HttpContext?.Session.Clear();
             return View("ServiceIssue", errorViewModel);
