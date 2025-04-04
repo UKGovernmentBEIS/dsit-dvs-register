531b77f9a465c7e4df694e9ed07b48af7e9d6e96
Aiswarya Rajendran <Aiswarya.Rajendran@dsit.gov.uk>
2025-04-04 19:56:08 +0100

    feat(db-changes): token status column addded

---
+++ DVSRegister.Data/Migrations/20250404094235_AddRemovalTokenStatusCoulmn.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DVSRegister.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRemovalTokenStatusCoulmn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RemovalTokenStatus",
+                table: "Service",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+
+            migrationBuilder.Sql(@"
+            UPDATE ""Service""
+            SET ""RemovalTokenStatus"" = CASE 
+                WHEN ""ServiceStatus"" = 6 OR ""ServiceStatus"" = 7   THEN 1                
+                WHEN ""ServiceStatus"" = 5 THEN 5               
+                ELSE 0
+            END
+        ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RemovalTokenStatus",
+                table: "Service");
+        }
+    }
+}
