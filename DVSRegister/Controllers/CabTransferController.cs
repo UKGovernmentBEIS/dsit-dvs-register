3e7d7e3b71abe9b5ebbe74e5e0f73461161d77f1
Kobithasan Vasantharajah <kobithasan.vasantharajah@uk.ey.com>
2025-05-19 09:45:56 +0100

    feat(cab-transfer): add CabTransferController and SelectConformityAssessmentBody Razor view

---
+++ DVSRegister/Controllers/CabTransferController.cs
@@ -0,0 +1,40 @@
+﻿using DVSRegister.BusinessLogic.Models.CAB;
+using DVSRegister.BusinessLogic.Services;
+using DVSRegister.CommonUtility;
+using DVSRegister.CommonUtility.JWT;
+using DVSRegister.CommonUtility.Models;
+using DVSRegister.CommonUtility.Models.Enums;
+using DVSRegister.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DVSRegister.Controllers
+{
+    [Route("cab-service/cab-transfer")]
+    public class CabTransferController : Controller
+    {
+        [HttpGet("select-cab")]
+        public IActionResult SelectConformityAssessmentBody()
+        {
+            return View();
+        }
+
+        [HttpPost("select-cab")]
+        public IActionResult SelectConformityAssessmentBodyPost()
+        {
+            var selectedCab = Request.Form["SelectedCab"].ToString();
+            if (string.IsNullOrEmpty(selectedCab))
+            {
+                ViewData["Error"] = "Select the CAB this service should be reassigned to";
+                return View("SelectConformityAssessmentBody");
+            }
+
+            return RedirectToAction("NextStep");
+        }
+
+        [HttpGet("next-step")]
+        public IActionResult NextStep()
+        {
+            return View("NextStep");
+        }
+    }
+}
