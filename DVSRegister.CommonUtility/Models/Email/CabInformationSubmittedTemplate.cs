002954e060400065e269bdf4238290f3513793c1
Aiswarya Rajendran <Aiswarya.Rajendran@dsit.gov.uk>
2025-05-30 12:25:13 +0100

    feat(cab-transfer): reject cab transfer request flow from service details

--- DVSRegister.CommonUtility/Models/Email/CabInformationSubmittedTemplate.cs
+++ DVSRegister.CommonUtility/Models/Email/CabInformationSubmittedTemplate.cs
@@ -4,5 +4,7 @@
     {
         public string Id { get; set; }
         public string RecipientName { get; set; }
+        public string ProviderName { get; set; }
+        public string ServiceName { get; set; }
     }
 }
