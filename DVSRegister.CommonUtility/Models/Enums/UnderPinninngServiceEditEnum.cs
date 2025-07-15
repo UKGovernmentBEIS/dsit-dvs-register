f2acc178ba8a9f2dfe51f8597df4aa5b0e3b3aa5
Aiswarya Rajendran <Aiswarya.Rajendran@dsit.gov.uk>
2025-07-15 22:00:21 +0100

    feat(db-changes): added underpinningserviceedittype enum column

---
+++ DVSRegister.CommonUtility/Models/Enums/UnderPinninngServiceEditEnum.cs
@@ -0,0 +1,15 @@
+﻿namespace DVSRegister.CommonUtility.Models
+{
+    //Donot change order of the enum as the ids are used to save in database
+    //New entries should be added at the last
+    public enum UnderPinninngServiceEditEnum
+    {
+        PublishedToPublished= 1,
+        PublishedToSelectOrChangeManual = 2,        
+        PublishedToEnterManual = 3,
+        ManualToPublished = 4,
+        ManualToSelectOrChangeManual = 5,
+        ManualToEnterManual = 6
+     
+    }
+}
