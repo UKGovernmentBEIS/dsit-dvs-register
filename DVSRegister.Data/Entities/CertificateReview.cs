b3a9fb2a5257a4a181bfdfc14e9f655cbdba5eee
Aiswarya Rajendran <Aiswarya.Rajendran@dsit.gov.uk>
2025-08-14 17:37:14 +0100

    feat(db-changes): bool columns for certificate review and pi check

--- DVSRegister.Data/Entities/CertificateReview.cs
+++ DVSRegister.Data/Entities/CertificateReview.cs
@@ -36,6 +36,7 @@ namespace DVSRegister.Data.Entities
         public bool? IsAuthenticyVerifiedCorrect { get; set; }
         public string? Comments { get; set; }
         public bool? InformationMatched { get; set; }
+        public bool? CertificateValid { get; set; }
         public string CommentsForIncorrect { get; set; }
         public string? RejectionComments { get; set; }
         public string? Amendments { get; set; }
---
+++ DVSRegister.Data/Migrations/20250814163031_AddReviewColumns.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DVSRegister.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReviewColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "PublicInterestChecksMet",
+                table: "PublicInterestCheck",
+                type: "boolean",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "CertificateValid",
+                table: "CertificateReview",
+                type: "boolean",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PublicInterestChecksMet",
+                table: "PublicInterestCheck");
+
+            migrationBuilder.DropColumn(
+                name: "CertificateValid",
+                table: "CertificateReview");
+        }
+    }
+}
