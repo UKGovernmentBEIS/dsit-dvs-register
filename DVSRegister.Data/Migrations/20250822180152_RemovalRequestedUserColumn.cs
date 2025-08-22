3b27540ed2dc25134caa3d1ae35a195b2746cd1e
Aiswarya Rajendran <Aiswarya.Rajendran@dsit.gov.uk>
2025-08-22 20:25:09 +0100

    feat(db-changes): column for removal requested user

---
+++ DVSRegister.Data/Migrations/20250822180152_RemovalRequestedUserColumn.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DVSRegister.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovalRequestedUserColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RemovalRequestedUser",
+                table: "Service",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RemovalRequestedUser",
+                table: "Service");
+        }
+    }
+}
