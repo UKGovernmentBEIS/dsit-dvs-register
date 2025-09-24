c7d593a06259b63ee81c6ffd5eb5b7b22afe33c2
Aiswarya Rajendran <Aiswarya.Rajendran@dsit.gov.uk>
2025-09-25 00:30:12 +0100

    fix(build-error):  email models

---
+++ DVSRegister.CommonUtility/Models/Email/CabTransfer/CabTransferConfirmationToDSIT.cs
@@ -0,0 +1,15 @@
+﻿using Notify.Models;
+
+namespace DVSRegister.CommonUtility.Models.Email
+{
+    public class CabTransferConfirmationToDSIT
+    {
+        public string Id { get; set; }
+        public string ExistingCabName { get; set; }
+        public string AcceptingCabName { get; set; }
+        public string ProviderName { get; set; }
+        public string ServiceName { get; set; }
+    }
+
+        
+}
