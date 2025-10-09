91314efcc7d5197dd5cef2bb6ac0b87c7341ef46
Aiswarya Rajendran <Aiswarya.Rajendran@dsit.gov.uk>
2025-10-09 10:58:40 +0100

    feat(db-changes): new table for provider removal request and service mapping

---
+++ DVSRegister.Data/Entities/ProviderRemovalRequestServiceMapping.cs
@@ -0,0 +1,25 @@
+﻿using DVSRegister.CommonUtility.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DVSRegister.Data.Entities
+{
+    public class ProviderRemovalRequestServiceMapping
+    {
+        public ProviderRemovalRequestServiceMapping() { }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [ForeignKey("ProviderRemovalRequest")]
+        public int ProviderRemovalRequestId { get; set; }
+        public ProviderRemovalRequest ProviderRemovalRequest { get; set; }
+
+        [ForeignKey("Service")]
+        public int ServiceId { get; set; }
+        public Service Service { get; set; }
+        public ServiceStatusEnum PreviousServiceStatus { get; set; }
+     
+    }
+}
