10a16d9bcffd5942038ff7b0949a3036a704b6b1
Megan Walsh <Megan.Walsh1@uk.ey.com>
2025-11-06 15:47:07 +0000

    feat(register-management): create new all providers page, add all providers to home controller

--- DVSRegister/Models/Register/AllProvidersViewModel.cs
+++ DVSRegister/Models/Register/AllProvidersViewModel.cs
@@ -1,11 +1,17 @@
 ﻿using DVSRegister.BusinessLogic.Models.CAB;
 using DVSRegister.Models.UI;
+using Microsoft.DotNet.Scaffolding.Shared;
 
 namespace DVSRegister.Models.Register
 {
     public class AllProvidersViewModel : PaginationAndFilteringParameters
     {
         public List<ProviderProfileDto>? Providers { get; set; }
-        
+        public string? CurrentSort { get; set; }
+        public string? CurrentSortAction { get; set; }
+
+
     }
+
+}
 }
