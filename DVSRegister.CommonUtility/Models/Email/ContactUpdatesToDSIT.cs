d10dadd9be8189323c08124873f8741d4c61bd3a
JoeGLauria <129402373+JoeGLauria@users.noreply.github.com>
2025-11-17 15:41:52 +0000

    feat(provider-edit): send email when the primary or secondary contact is changed by the cab

---
+++ DVSRegister.CommonUtility/Models/Email/ContactUpdatesToDSIT.cs
@@ -0,0 +1,10 @@
+﻿namespace DVSRegister.CommonUtility.Models.Email
+{
+    public class ContactUpdatesToDSIT
+    {
+        public string Id { get; set; }
+        public string ProviderName { get; set; }
+        public string PreviousData { get; set; }
+        public string CurrentData { get; set; }
+    }
+}
