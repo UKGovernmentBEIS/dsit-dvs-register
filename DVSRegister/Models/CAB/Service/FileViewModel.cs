19775d3aa9c87710ab76cce31d354a6094bd7d11
JoeGLauria <joe.lauria@uk.ey.com>
2026-02-11 15:42:50 +0000

    feat(pen-testing): implement content-type allow-list and PDF signature validation

--- DVSRegister/Models/CAB/Service/FileViewModel.cs
+++ DVSRegister/Models/CAB/Service/FileViewModel.cs
@@ -10,21 +10,21 @@ namespace DVSRegister.Models.CAB
         public bool? FileUploadedSuccessfully { get; set; }
         public bool FileRemoved { get; set; }
     }
-
     public class CertificateFileViewModel : FileViewModel
     {
         [Required(ErrorMessage = "Upload the digital identity and attribute service provider's certificate of conformity")]
         [MaxFileSize(5)]
         [AllowedExtensions(new[] { ".pdf" }, ErrorMessage = "The selected file must be a PDF")]
+        [AllowedContentTypes(new[] { "application/pdf" }, ErrorMessage = "The selected file must be a PDF")]
         public IFormFile? File { get; set; }
     }
-
     public class TOUFileViewModel : FileViewModel
     {
         [Required(ErrorMessage = "Upload the Terms of Use for the UK CertifID trust mark")]
         [MaxFileSize(5)]
         [AllowedExtensions(new[] { ".pdf" }, ErrorMessage = "The selected file must be a PDF")]
+        [AllowedContentTypes(new[] { "application/pdf" }, ErrorMessage = "The selected file must be a PDF")]
         public IFormFile? File { get; set; }
-    }
+    }  
 }
 
