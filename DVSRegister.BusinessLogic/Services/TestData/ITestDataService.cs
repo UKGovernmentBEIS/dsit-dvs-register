c94178d8e11baa7bc89e13dccfc0193e546c74ef
Aiswarya Rajendran <Aiswarya.Rajendran@dsit.gov.uk>
2026-04-22 12:02:23 +0100

    feat(testing): provider and service test data creation

---
+++ DVSRegister.BusinessLogic/Services/TestData/ITestDataService.cs
@@ -0,0 +1,10 @@
+﻿using DVSRegister.BusinessLogic.Models.CAB;
+
+namespace DVSRegister.BusinessLogic.Services.TestData
+{
+    public interface ITestDataService
+    {
+        public Task CreateTestProvider(string loggedInUserEmail, bool allowTestDataCreation, ProviderProfileDto providerProfileDto);
+        public Task CreateTestServices(string loggedInUserEmail, bool allowTestDataCreation, int providerId, List<ServiceDto> services);
+    }
+}
