880b8171f2e996438e326e11b977fb0a913e2fbb
Aiswarya Rajendran <Aiswarya.Rajendran@dsit.gov.uk>
2026-05-18 14:00:59 +0100

    feat(db-changes): tables for account management

---
+++ DVSRegister.CommonUtility/Models/Enums/UserRoleEnum.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace DVSRegister.CommonUtility.Models.Enums
+{
+    //Ofdia user roles
+    public enum UserRoleEnum
+    {
+        [Description("Member")]
+        Member = 1,
+        [Description("Manager")]
+        Manager = 2
+    }
+}
